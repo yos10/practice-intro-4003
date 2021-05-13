be50cfe83f01cffe34abc8d7f3e2eb7f6ad2c981
yos10 <61726023+yos10@users.noreply.github.com>
2021-05-13 19:21:44 +0900

    WIP

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var helmet = require('helmet');
-var session = require('express-session');
-var passport = require('passport');
-var GitHubStrategy = require('passport-github2').Strategy;
-var { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, EXPRESS_SESSION_SECRET } = require('./config/config');
+'use strict';
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const session = require('express-session');
+const router = require('./routes/routes');
+const passport = require('./middleware/passport');
 
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
-});
-
-passport.use(new GitHubStrategy({
-  clientID: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
-},
-  function (accessToken, refreshToken, profile, done) {
-    process.nextTick(function () {
-      return done(null, profile);
-    });
-  }
-));
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var photosRouter = require('./routes/photos');
-
-var app = express();
+const app = express();
 app.use(helmet());
 
 // view engine setup
@@ -46,40 +24,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ secret: EXPRESS_SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', ensureAuthenticated, usersRouter);
-app.use('/photos', photosRouter);
-
-app.get('/auth/github',
-  passport.authenticate('github', { scope: ['user:email'] }),
-  function (req, res) {
-  });
-
-app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function (req, res) {
-    res.redirect('/');
-  });
-
-app.get('/login', function (req, res) {
-  res.render('login');
-});
-
-app.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
-});
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-}
+// ルーティング
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
